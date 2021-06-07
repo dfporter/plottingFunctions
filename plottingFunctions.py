ad247ad081c3a3c7b94e1082f29e37801ef47b8a
Douglas Porter <dfporter@users.noreply.github.com>
2021-06-06 18:47:08 -0700

    Update plottingFunctions.py
    Better default.

--- plottingFunctions.py
+++ plottingFunctions.py
@@ -14,10 +14,11 @@ matplotlib.rcParams['font.family'] = 'sans-serif'
 matplotlib.rcParams['font.sans-serif'] = 'Arial'
 
 def label_outliers(
-    _x, _y, _val, proximity_cutoff=1, ax=None, n_points_too_close_cutoff=3, fontsize=5, verbose=False):
+    _x, _y, _val, proximity_cutoff=None, scale_cutoff=1, ax=None, n_points_too_close_cutoff=3, fontsize=5, verbose=False):
     
-    if proximity_cutoff < 0:  # Estimate from data.
-        proximity_cutoff = ((np.std(_x) * np.std(_y))**0.5)/2
+    if proximity_cutoff is None:  # Estimate from data.
+        xspan,yspan = (np.max(_x)-np.min(_x), np.max(_y)-np.min(_y))
+        proximity_cutoff = ((xspan*yspan)**0.5)/(20/scale_cutoff)
         verbose and (print(f"Estimating a proximiting cufoff {proximity_cutoff} because proximity_cutoff<0"))
 
     (ax is None) and print(f"ax was not given - this function does nothing without the ax parameter")
