cf6c8a4f272240c95252f592d2c5926557ac5309
Douglas Porter <dfporter@gmail.com>
2022-09-15 13:29:57 -0700

    adjustText now used to add arrows and move labels around

--- README.md
+++ README.md
@@ -1,6 +1,37 @@
-Reused plotting functions.
+## Label points on a matplotlib graph with adjustments and arrows as needed.
 
-plottingFunctions.py inclused code to label outlier dots in a scatterplot.
+Uses adjustText to find the spot to place text and arrows.
+adjustText did not have the options to plot a subset of points based on how many neighbors were near, or to plot an arrow only if needed.
+
+Set n_points_too_close_to_label to not plot labels for points with too many neighbors (default 4).
+Set n_points_too_close_to_need_arrow to not include an arrow for points with few neighbors (default 1).
+
+If arrowprops is not False (default), labels with an arrow. 
+adjustText is used to plot the text labels.
+Extra keyword arguments are passed to adjustText.adjust_text.
+A higher scale_cutoff is a higher proximity cutoff (fewer labels).
+For faster plotting of huge numbers of datapoints, use no_adjust=True.
+
+Requires:
+* sklearn
+* numpy
+* matplotlib
+* adjustText (https://github.com/Phlya/adjustText)
+
+Example:
+```
+import plottingFunctions
+q = pandas.DataFrame(np.random.gamma(1,.1, size=(80, 2)), columns=list('xy'))
+fig, ax = plt.subplots()
+sns.scatterplot(data=q, x='x', y='y')
+plottingFunctions.label_outliers(
+    q['x'], q['y'], q.index, scale_cutoff=.8, n_points_too_close_to_label=3, n_points_too_close_to_need_arrow=1, 
+    arrowprops={'arrowstyle':'-', 'color':'red', 'alpha':.8, 'lw':1},
+    ax=ax, fontsize=10)
+plt.show(); plt.clf()
+```
+![alt text](example.png)
+
 cluster_dataframe.py includes the clustering algorithm code extracted from seaborn's clustermap function.
 
 Install:
--- plottingFunctions/plottingFunctions.py
+++ plottingFunctions/plottingFunctions.py
@@ -1,7 +1,7 @@
 from sklearn.neighbors import NearestNeighbors
 import numpy as np
-
 import matplotlib
+import adjustText
 
 # Import the following to allow matplotlib to find sans-serif on CentOS.
 import matplotlib.font_manager
@@ -13,8 +13,17 @@ matplotlib.rcParams['ps.fonttype'] = 42
 matplotlib.rcParams['font.family'] = 'sans-serif'
 matplotlib.rcParams['font.sans-serif'] = 'Arial'
 
+
 def label_outliers(
-    _x, _y, _val, proximity_cutoff=None, scale_cutoff=1, ax=None, n_points_too_close_cutoff=3, dodge=0, fontsize=5, verbose=False):
+    _x, _y, _val, proximity_cutoff=None, scale_cutoff=1, ax=None, n_points_too_close_to_label=4, 
+    n_points_too_close_to_need_arrow=1, no_adjust=False, dodge=0,
+    arrowprops={'arrowstyle':'-', 'color':'gray', 'alpha':.5}, fontsize=5, verbose=False, **adjust_text_kwargs):
+    """Label points on a graph. Identifies points with few neighbors and points without an arrow.
+    If arrowprops is not False (default), label with an arrow. adjustText is used to plot the text labels.
+    Extra keyword arguments are passed to adjustText.adjust_text.
+    A higher scale_cutoff is a higher proximity cutoff (fewer labels).
+    For faster plotting of huge numbers of datapoints, use no_adjust=True.
+    The dodge option should only be used if no_adjust=True."""
     
     if proximity_cutoff is None:  # Estimate from data.
         xspan,yspan = (np.max(_x)-np.min(_x), np.max(_y)-np.min(_y))
@@ -24,14 +33,33 @@ def label_outliers(
     (ax is None) and print(f"ax was not given - this function does nothing without the ax parameter")
         
     points = np.array([(x,y) for x,y in zip(_x, _y)])
+    
+    points = points[~np.isnan(points).any(axis=1), :]
+    points = points[np.isfinite(points).all(axis=1), :]
+
     nbrs = NearestNeighbors(n_neighbors=min([len(points), 10])).fit(points)
     distances, indices = nbrs.kneighbors(points)
-    closest_points = distances[:, 1:n_points_too_close_cutoff+2]
+          
+    closest_points = distances[:, 1:n_points_too_close_to_label+2]
     n_points_close = [len(x[x<proximity_cutoff]) for x in closest_points]
+          
+    textsNoArrow, textsWithArrow = [], []
+    for (x, y, val, n_points_close_to_this_dot) in zip(_x, _y, _val, n_points_close):
+        
+        if np.any(~np.isfinite([x, y])):
+            continue
+        
+        if n_points_close_to_this_dot < n_points_too_close_to_need_arrow:
+            t = ax.text(x + dodge, y + dodge, val, size=fontsize)
+            textsNoArrow.append(t)
+            
+        elif n_points_close_to_this_dot < n_points_too_close_to_label:
+            t = ax.text(x + dodge, y + dodge, val, size=fontsize)
+            textsWithArrow.append(t)
     
-    for (x, y, val, n_points_close) in zip(_x, _y, _val, n_points_close):
-        if n_points_close >= n_points_too_close_cutoff:
-            continue  # Don't label.
-        if ax is not None:
-            ax.text(x+dodge, y+dodge, val, size=fontsize)
-
+    if not no_adjust:
+        if len(textsNoArrow):
+            adjustText.adjust_text(textsNoArrow, ax=ax, **adjust_text_kwargs)
+        if len(textsWithArrow):
+            adjustText.adjust_text(textsWithArrow, ax=ax, arrowprops=arrowprops, **adjust_text_kwargs)
+    
